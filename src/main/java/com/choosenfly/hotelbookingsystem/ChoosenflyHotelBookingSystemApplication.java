349015567fb567c60ff7e8e986e810b344bbe03e
orell@123 <sijinorell@gmail.com>
2025-05-16 13:50:19 +0530

    commiting before cloning to personal lap (subin)

--- src/main/java/com/choosenfly/hotelbookingsystem/ChoosenflyHotelBookingSystemApplication.java
+++ src/main/java/com/choosenfly/hotelbookingsystem/ChoosenflyHotelBookingSystemApplication.java
@@ -4,12 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.retry.annotation.EnableRetry;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@OpenAPIDefinition(
+	    info = @Info(title = "Hotel Booking API", version = "1.0", description = "API for Hotel CRUD")
+	)
 @SpringBootApplication
 @EnableRetry
 public class ChoosenflyHotelBookingSystemApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ChoosenflyHotelBookingSystemApplication.class, args);
+		SpringApplication.run(ChoosenflyHotelBookingSystemApplication.class, args); 
 	}
 
 }
