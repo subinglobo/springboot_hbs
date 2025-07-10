c47324fa40562387f2c66f7094bd5f7d56a55d32
Subin Suresh <sonusubin2@gmail.com>
2025-07-10 11:17:12 +0530

    merged arathi crud changes

--- src/main/java/com/choosenfly/hotelbookingsystem/entities/master/MasterBank.java
+++ src/main/java/com/choosenfly/hotelbookingsystem/entities/master/MasterBank.java
@@ -14,7 +14,7 @@ import jakarta.persistence.Table;
 public class MasterBank extends BaseEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY) 
     @Column(name = "bank_id", nullable = false)
     private Long bankId;
 
