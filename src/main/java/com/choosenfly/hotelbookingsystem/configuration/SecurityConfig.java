bc39d2371332e67acc1fbc067e12a4757b641b21
Subin Suresh <sonusubin2@gmail.com>
2025-08-05 17:00:44 +0530

    cors changes added

--- src/main/java/com/choosenfly/hotelbookingsystem/configuration/SecurityConfig.java
+++ src/main/java/com/choosenfly/hotelbookingsystem/configuration/SecurityConfig.java
@@ -44,12 +44,12 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
+        .cors(cors -> cors.configurationSource(corsConfigurationSource()))
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/auth/login", "/auth/register").permitAll()
                 .anyRequest().authenticated())
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
-
         return http.build();
     }
 
