12a0e97363236f5c0362706556d6c5cbdd707c34
Subin Suresh <sonusubin2@gmail.com>
2025-08-05 11:22:48 +0530

    Cors Added

--- src/main/java/com/choosenfly/hotelbookingsystem/configuration/SecurityConfig.java
+++ src/main/java/com/choosenfly/hotelbookingsystem/configuration/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.choosenfly.hotelbookingsystem.configuration;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +14,9 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.choosenfly.hotelbookingsystem.auth.util.jwt.CustomUserDetailsService;
 import com.choosenfly.hotelbookingsystem.auth.util.jwt.JwtAuthenticationFilter;
@@ -51,5 +56,18 @@ public class SecurityConfig {
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
         return config.getAuthenticationManager();
+    }
+    
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(Arrays.asList("*"));
+        config.setAllowCredentials(true); // If sending cookies or auth headers
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
     }
 }
