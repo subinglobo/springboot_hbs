62804972730c103e186dfefb26fbe7b9ddc70b99
orell@123 <sijinorell@gmail.com>
2025-08-07 11:56:49 +0530

    securit config changes to add agent category list for register

--- src/main/java/com/choosenfly/hotelbookingsystem/configuration/SecurityConfig.java
+++ src/main/java/com/choosenfly/hotelbookingsystem/configuration/SecurityConfig.java
@@ -46,7 +46,7 @@ public class SecurityConfig {
         http.csrf(csrf -> csrf.disable())
         .cors(cors -> cors.configurationSource(corsConfigurationSource()))
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/auth/login", "/api/agent/register","/api/province/getByCountryId/*","/api/country" ,"/api/destination/getplaces/*").permitAll()
+                .requestMatchers("/auth/login", "/api/agent/register","/api/province/getByCountryId/*","/api/country" ,"/api/destination/getplaces/*" , "/api/agentCategory").permitAll()
                 .anyRequest().authenticated())
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
