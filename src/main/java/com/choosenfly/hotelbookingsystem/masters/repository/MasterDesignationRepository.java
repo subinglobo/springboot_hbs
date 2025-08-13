a50e4a1c50cf8ff5d0b7dedff25b1807f8ef2839
orell@123 <sijinorell@gmail.com>
2025-08-13 15:33:12 +0530

    designation list crud done

--- src/main/java/com/choosenfly/hotelbookingsystem/masters/repository/MasterDesignationRepository.java
+++ src/main/java/com/choosenfly/hotelbookingsystem/masters/repository/MasterDesignationRepository.java
@@ -1,5 +1,7 @@
 package com.choosenfly.hotelbookingsystem.masters.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +10,7 @@ import com.choosenfly.hotelbookingsystem.masters.entities.MasterDesignation;
 
 @Repository
 public interface MasterDesignationRepository extends JpaRepository<MasterDesignation, Long>{
+
+	Page<MasterDesignation> findByNameContainingIgnoreCase(String search, Pageable pageable);
 
 }
